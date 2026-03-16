9538e38fcbf25cc774dda6d686d71314c6552b56
Steppedcrown <steppedcrown@gmail.com>
2026-03-16 15:38:40 -0700

    Stop tracking build artifacts and macOS metadata (builds/ + .DS_Store)

--- builds/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aidenwaldorf/Developer/CMPM 123/chess-123
-# Build directory: /Users/aidenwaldorf/Developer/CMPM 123/chess-123/builds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
